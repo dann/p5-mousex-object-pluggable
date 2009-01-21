040d58aa0204cd567664a8dfb6a0178d06488e1a
Dann <techmemo@gmail.com>
2009-01-22 04:42:25 +0900

    initial commit

---
+++ lib/MouseX/Object/Pluggable.pm
@@ -0,0 +1,33 @@
+package MouseX::Object::Pluggable;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+MouseX::Object::Pluggable -
+
+=head1 SYNOPSIS
+
+  use MouseX::Object::Pluggable;
+
+=head1 DESCRIPTION
+
+MouseX::Object::Pluggable is
+
+=head1 AUTHOR
+
+Takatoshi Kitano E<lt>kitano.tk@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
